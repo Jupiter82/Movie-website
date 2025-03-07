06392c2889cfab08333fe2437e5872a8e26684c0
Jupiter82 <jupiterbade@gmail.com>
2025-03-07 23:42:18 +0545

    new project design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css";
+import "./css/App.css";
 import Home from "../src/pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import MovieCard from "../components/MovieCard";
 import { useState } from "react";
-
+import "../css/Home.css";
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
-    { id: 2, title: "John Wick2", release_date: "2022" },
-    { id: 3, title: "John Wick3", release_date: "2024" },
+    { id: 2, title: "Wick2", release_date: "2022" },
+    { id: 3, title: "ohn Wick3", release_date: "2024" },
   ];
 
   const handleSearch = (e) => {
